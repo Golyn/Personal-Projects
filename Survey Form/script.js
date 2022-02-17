9fa902479e9d6b582eca4e92878323251a8d7fa1
Seth Amankwah <53409423+Golyn@users.noreply.github.com>
2022-02-17 21:27:39 +0000

    Add files via upload

---
+++ Survey Form/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const button = document.getElementById("submit");
+button.addEventListener("click", function () {
+  button.style.backgroundColor = "#6BCDFF";
+  alert("Form submitted successfully!");
+});
